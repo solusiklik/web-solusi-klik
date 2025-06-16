eb9e42a1fcb9536e93ec98113649599205b7e42f
recka <recka309@gmail.com>
2025-06-16 13:37:25 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-primary">Hello Bootstrap!</h1>
+    </div>
+  );
+}
+
+export default App;
